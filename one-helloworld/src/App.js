a2bdc7c006481e0438b39438842f08a359f3f019
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-02 22:33:36 +0530

    Hello World in React

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+import Button from "./Buttons";
+
+
+const App = () => {
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>Hello World!</h1>
+            <Button title="App Store"/>
+            <Button title="Play Store"/>
+            <Button />
+        </div>
+    )
+}
+
+export default App;
